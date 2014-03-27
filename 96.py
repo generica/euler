a230a2344065e13603e5aa428561157db85ecf41
Brett Pemberton <brett@pemberton.at>
2014-03-28 10:25:16 +1100

    Clean up generation, grid printing, pep8

--- 96.py
+++ 96.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
-box = {0: [0,1,2,9,10,11,18,19,20], 1: [3,4,5,12,13,14,21,22,23], 2: [6,7,8,15,16,17,24,25,26], 3: [27,28,29,36,37,38,45,46,47], 4: [30,31,32,39,40,41,48,49,50], 5: [33,34,35,42,43,44,51,52,53], 6: [54,55,56,63,64,65,72,73,74], 7: [57,58,59,66,67,68,75,76,77], 8: [60,61,62,69,70,71,78,79,80]}
+box = {}
+j = 0
+for i in range(0, 7, 3) + range(27, 34, 3) + range(54, 61, 3):
+    box[j] = [i + x for x in [0, 1, 2, 9, 10, 11, 18, 19, 20]]
+    j += 1
+print box
 row = {}
 for i in range(0, 9):
     row[i] = [j for j in range(i * 9, (i * 9) + 9)]
@@ -12,7 +17,11 @@ layout = {'box': box, 'col': col, 'row': row}
 
 def print_grid(grid):
     for x in range(0, 81, 9):
-        print "%s" % (grid[x:x+9])
+        if x % 27 == 0 and x != 0:
+            print "---|---|---"
+        print "%s|%s|%s" % (grid[x:x + 3], grid[x + 3:x + 6], grid[x + 6:x + 9])
+    print
+
 
 def print_possibles(possibles):
     for x in range(0, 81):
@@ -35,12 +44,13 @@ def find_sole(grid, possibles, direction, n):
                 counts[digit] = 1
 
     for digit in counts:
-         if counts[digit] == 1:
+        if counts[digit] == 1:
             for x in layout[direction][n]:
                 if digit in possibles[x]:
-                    possibles[x] = [digit]                
+                    possibles[x] = [digit]
 
     return (grid, possibles)
+
 
 def remove(grid, possibles, direction, n):
 
@@ -97,6 +107,15 @@ def is_solved(possibles):
     return True
 
 
+def possibles_to_grid(possibles):
+
+    grid = ""
+
+    for x in range(0, 81):
+        grid += str(possibles[x][0])
+    return grid
+
+
 def solve(grid):
 
     possibles = {}
@@ -124,7 +143,8 @@ def solve(grid):
                 (grid, possibles) = dupes(grid, possibles, x, i)
 
         if is_solved(possibles):
-            print_possibles(possibles)
+            grid = possibles_to_grid(possibles)
+            print_grid(grid)
             return 0
         else:
             tally += 1
